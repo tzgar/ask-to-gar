017e778c64e26e613edc356496d1ac672010dcf4
tegar-iz <120255128+tzgar@users.noreply.github.com>
2024-08-17 06:34:15 +0700

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,1 +1,20 @@
+const socket = io()
+let textarea = document.querySelector('#textarea')
 
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter') {
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(message) {
+    let msg = {
+        user: "?",
+        message: message.trim()
+    }
+    textarea.value = ''
+    console.log(msg.message)
+    // Send to server 
+    socket.emit('message', msg)
+
+}
